6c99c1e35de8e4d515c89373339c9562e8b66786
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-05-20 03:37:29 -0700

    wrote code

--- models/Product.js
+++ models/Product.js
@@ -10,13 +10,53 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+    id:
+    {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name:
+    {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price:
+    {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate:
+      {
+        isDecimal: true
+      }
+    },
+    stock:
+    {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaulValue: 10,
+      validate:
+      {
+        isNumeric: true
+      }
+    },
+    category_id:
+    {
+      type: DataTypes.INTEGER,
+      references:
+      {
+        model: 'category',
+        key: 'id'
+    }
+  }
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
   }
 );
 
